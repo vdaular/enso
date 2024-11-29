dc55b4e9ce272918b1846017ff8f32e526014c8e
Radosław Waśko <radoslaw.wasko@enso.org>
2024-11-29 12:06:30 +0000

    Support for `Table` output in `Data.read_many` (#11546)
    - Closes #11311

--- std-bits/base/src/main/java/org/enso/base/read/ReadManyReturnSPI.java
+++ std-bits/base/src/main/java/org/enso/base/read/ReadManyReturnSPI.java
@@ -4,6 +4,15 @@ import java.util.ServiceLoader;
 import org.enso.base.polyglot.EnsoMeta;
 import org.graalvm.polyglot.Value;
 
+/**
+ * An SPI for specifying return types to the `read_many` operation.
+ *
+ * <p>The `read_many` operation can take return types provided from various libraries. This SPI
+ * ensures that it can be aware of all the available types from the loaded libraries. If a library
+ * registers a return type here, it will be available for autoscoping resolution and will appear in
+ * the dropdown. Registered types must provide methods `get_dropdown_options`, `resolve` and
+ * `make_return`. See `Standard.Base.Data.Read.Return_As` for examples.
+ */
 public abstract class ReadManyReturnSPI {
   private static final ServiceLoader<ReadManyReturnSPI> loader =
       ServiceLoader.load(ReadManyReturnSPI.class, ReadManyReturnSPI.class.getClassLoader());
