f7ca795dbe0348bab0628d0adc1dcd75eaa31b76
somebody1234 <ehern.lee@gmail.com>
2025-01-03 00:57:25 +1000

    Fix build error caused by Sentry Vite plugin (#11961)
    Fix parsing error causing Sentry builds to fail
    
    # Important Notes
    None

--- app/gui/vite.config.ts
+++ app/gui/vite.config.ts
@@ -77,7 +77,6 @@ export default defineConfig({
         sentryVitePlugin({
           org: process.env.ENSO_CLOUD_SENTRY_ORGANIZATION,
           project: process.env.ENSO_CLOUD_SENTRY_PROJECT,
-          reactComponentAnnotation: { enabled: true },
           ...(process.env.ENSO_VERSION != null ?
             { release: { name: process.env.ENSO_VERSION } }
           : {}),
