vdaular/enso

commit 25933afeec1d942598bd43f2765ce134bd5332eb

Batch asset invalidations (#11937)
- Close https://github.com/enso-org/cloud-v2/issues/1627
- Batch asset invalidations for:
- Bulk deletes
- Bulk undo deletes (restores)
- Bulk copy/move
- Bulk download
- This avoids flickering when the directory list is invalidated multiple times (once for the mutation corresponding to each asset)

Codebase changes:
- Remove all `AssetEvent`s and `AssetListEvent`s. Remaining events have been moved to TanStack Query mutations in this PR, as it is neccessary for batch invalidation functionality.
- Remove `key` and `directoryKey` from `AssetTreeNode`, and `key`s in general in favor of `id`s. Not *strictly* necessary, but it was causing logic issues and is (IMO) a nice simplification to be able to do.

# Important Notes
None

app/gui/src/dashboard/layouts/Drive/assetTreeHooks.tsx
@@ -1,10 +1,11 @@
 /** @file A hook to return the asset tree. */
 import { useMemo } from 'react'
 
-import { useIsFetching, useQueries, useQuery, useQueryClient } from '@tanstack/react-query'
+import { useIsFetching, useQueries, useQueryClient } from '@tanstack/react-query'
 
 import type { DirectoryId } from 'enso-common/src/services/Backend'
 import {
+  BackendType,
   assetIsDirectory,
   createRootDirectoryAsset,
   createSpecialEmptyAsset,
@@ -17,10 +18,9 @@ import {
 import { listDirectoryQueryOptions } from '#/hooks/backendHooks'
 import { useEventCallback } from '#/hooks/eventCallbackHooks'
 import type { Category } from '#/layouts/CategorySwitcher/Category'
+import { useRefetchDirectories } from '#/layouts/Drive/fetchDirectoriesHooks'
 import { useFullUserSession } from '#/providers/AuthProvider'
 import { useBackend } from '#/providers/BackendProvider'
-import { useFeatureFlag } from '#/providers/FeatureFlagsProvider'
-import { ROOT_PARENT_DIRECTORY_ID } from '#/services/remoteBackendPaths'
 import AssetTreeNode, { type AnyAssetTreeNode } from '#/utilities/AssetTreeNode'
 
 /** Return type of the query function for the `listDirectory` query. */
@@ -42,10 +42,8 @@ export function useAssetTree(options: UseAssetTreeOptions) {
 
   const backend = useBackend(category)
 
-  const enableAssetsTableBackgroundRefresh = useFeatureFlag('enableAssetsTableBackgroundRefresh')
-  const assetsTableBackgroundRefreshInterval = useFeatureFlag(
-    'assetsTableBackgroundRefreshInterval',
-  )
+  useRefetchDirectories(BackendType.local)
+  useRefetchDirectories(BackendType.remote)
 
   const directories = useQueries({
     // We query only expanded directories, as we don't want to load the data for directories that are not visible.
@@ -86,44 +84,7 @@ export function useAssetTree(options: UseAssetTreeOptions) {
 
   const queryClient = useQueryClient()
 
-  // We use a different query to refetch the directory data in the background.
-  // This reduces the amount of rerenders by batching them together, so they happen less often.
-  const { refetch } = useQuery(
-    useMemo(
-      () => ({
-        queryKey: [backend.type, 'refetchListDirectory'],
-        queryFn: () =>
-          queryClient
-            .refetchQueries({
-              queryKey: [backend.type, 'listDirectory'],
-              type: 'active',
-            })
-            .then(() => null),
-        refetchInterval:
-          enableAssetsTableBackgroundRefresh ? assetsTableBackgroundRefreshInterval : false,
-        refetchOnMount: 'always',
-        refetchIntervalInBackground: false,
-        refetchOnWindowFocus: true,
-        enabled: !hidden,
-        meta: { persist: false },
-      }),
-      [
-        backend.type,
-        enableAssetsTableBackgroundRefresh,
-        assetsTableBackgroundRefreshInterval,
-        hidden,
-        queryClient,
-      ],
-    ),
-  )
-
-  const refetchAllDirectories = useEventCallback(() => {
-    return refetch()
-  })
-
-  /**
-   * Refetch the directory data for a given directory.
-   */
+  /** Refetch the directory data for a given directory. */
   const refetchDirectory = useEventCallback((directoryId: DirectoryId) => {
     return queryClient.refetchQueries({
       queryKey: listDirectoryQueryOptions({
@@ -149,36 +110,14 @@ export function useAssetTree(options: UseAssetTreeOptions) {
     // If the root directory is not loaded, then we cannot render the tree.
     // Return null, and wait for the root directory to load.
     if (rootDirectoryContent == null) {
-      return AssetTreeNode.fromAsset(
-        createRootDirectoryAsset(rootDirectory.id),
-        ROOT_PARENT_DIRECTORY_ID,
-        ROOT_PARENT_DIRECTORY_ID,
-        -1,
-        rootPath,
-        null,
-      )
+      return AssetTreeNode.fromAsset(createRootDirectoryAsset(rootDirectory.id), -1, rootPath)
     } else if (isError) {
-      return AssetTreeNode.fromAsset(
-        createRootDirectoryAsset(rootDirectory.id),
-        ROOT_PARENT_DIRECTORY_ID,
-        ROOT_PARENT_DIRECTORY_ID,
-        -1,
-        rootPath,
-        null,
-      ).with({
-        children: [
-          AssetTreeNode.fromAsset(
-            createSpecialErrorAsset(rootDirectory.id),
-            rootDirectory.id,
-            rootDirectory.id,
-            0,
-            '',
-          ),
-        ],
-      })
+      return AssetTreeNode.fromAsset(createRootDirectoryAsset(rootDirectory.id), -1, rootPath).with(
+        {
+          children: [AssetTreeNode.fromAsset(createSpecialErrorAsset(rootDirectory.id), 0, '')],
+        },
+      )
     }
-
-    const rootId = rootDirectory.id
 
     const children = rootDirectoryContent.map((content) => {
       /**
@@ -193,52 +132,20 @@ export function useAssetTree(options: UseAssetTreeOptions) {
           const childrenAssetsQuery = directories.directories.get(item.id)
 
           const nestedChildren = childrenAssetsQuery?.data?.map((child) =>
-            AssetTreeNode.fromAsset(
-              child,
-              item.id,
-              item.id,
-              depth,
-              `${node.path}/${child.title}`,
-              null,
-              child.id,
-            ),
+            AssetTreeNode.fromAsset(child, depth, `${node.path}/${child.title}`),
           )
 
           if (childrenAssetsQuery == null || childrenAssetsQuery.isLoading) {
             node = node.with({
-              children: [
-                AssetTreeNode.fromAsset(
-                  createSpecialLoadingAsset(item.id),
-                  item.id,
-                  item.id,
-                  depth,
-                  '',
-                ),
-              ],
+              children: [AssetTreeNode.fromAsset(createSpecialLoadingAsset(item.id), depth, '')],
             })
           } else if (childrenAssetsQuery.isError) {
             node = node.with({
-              children: [
-                AssetTreeNode.fromAsset(
-                  createSpecialErrorAsset(item.id),
-                  item.id,
-                  item.id,
-                  depth,
-                  '',
-                ),
-              ],
+              children: [AssetTreeNode.fromAsset(createSpecialErrorAsset(item.id), depth, '')],
             })
           } else if (nestedChildren?.length === 0) {
             node = node.with({
-              children: [
-                AssetTreeNode.fromAsset(
-                  createSpecialEmptyAsset(item.id),
-                  item.id,
-                  item.id,
-                  depth,
-                  '',
-                ),
-              ],
+              children: [AssetTreeNode.fromAsset(createSpecialEmptyAsset(item.id), depth, '')],
             })
           } else if (nestedChildren != null) {
             node = node.with({
@@ -250,30 +157,13 @@ export function useAssetTree(options: UseAssetTreeOptions) {
         return node
       }
 
-      const node = AssetTreeNode.fromAsset(
-        content,
-        rootId,
-        rootId,
-        0,
-        `${rootPath}/${content.title}`,
-        null,
-        content.id,
-      )
+      const node = AssetTreeNode.fromAsset(content, 0, `${rootPath}/${content.title}`)
 
       const ret = withChildren(node, 1)
       return ret
     })
 
-    return new AssetTreeNode(
-      rootDirectory,
-      ROOT_PARENT_DIRECTORY_ID,
-      ROOT_PARENT_DIRECTORY_ID,
-      children,
-      -1,
-      rootPath,
-      null,
-      rootId,
-    )
+    return new AssetTreeNode(rootDirectory, children, -1, rootPath)
   }, [
     backend,
     category,
@@ -290,6 +180,5 @@ export function useAssetTree(options: UseAssetTreeOptions) {
     assetTree,
     isFetching,
     refetchDirectory,
-    refetchAllDirectories,
   } as const
 }

app/gui/src/dashboard/layouts/Drive/assetsTableItemsHooks.tsx
@@ -182,12 +182,12 @@ export function useAssetsTableItems(options: UseAssetsTableOptions) {
 
       for (const child of node.children ?? []) {
         if (visible && child.item.type === AssetType.specialEmpty) {
-          map.set(child.key, Visibility.visible)
+          map.set(child.item.id, Visibility.visible)
         } else {
           processNode(child)
         }
 
-        if (map.get(child.key) !== Visibility.hidden) {
+        if (map.get(child.item.id) !== Visibility.hidden) {
           displayState = Visibility.faded
         }
       }
@@ -196,7 +196,7 @@ export function useAssetsTableItems(options: UseAssetsTableOptions) {
         displayState = Visibility.visible
       }
 
-      map.set(node.key, displayState)
+      map.set(node.item.id, displayState)
 
       return displayState
     }
@@ -209,7 +209,7 @@ export function useAssetsTableItems(options: UseAssetsTableOptions) {
   const displayItems = useMemo(() => {
     if (sortInfo == null) {
       const flatTree = assetTree.preorderTraversal((children) =>
-        children.filter((child) => expandedDirectoryIds.includes(child.directoryId)),
+        children.filter((child) => expandedDirectoryIds.includes(child.item.parentId)),
       )
 
       return flatTree
@@ -231,7 +231,9 @@ export function useAssetsTableItems(options: UseAssetsTableOptions) {
         }
       }
       const flatTree = assetTree.preorderTraversal((tree) =>
-        [...tree].filter((child) => expandedDirectoryIds.includes(child.directoryId)).sort(compare),
+        [...tree]
+          .filter((child) => expandedDirectoryIds.includes(child.item.parentId))
+          .sort(compare),
       )
 
       return flatTree
@@ -241,7 +243,7 @@ export function useAssetsTableItems(options: UseAssetsTableOptions) {
   setAssetItems(displayItems.map((item) => item.item))
 
   const visibleItems = useMemo(
-    () => displayItems.filter((item) => visibilities.get(item.key) !== Visibility.hidden),
+    () => displayItems.filter((item) => visibilities.get(item.item.id) !== Visibility.hidden),
     [displayItems, visibilities],
   )
 

commit 0970a2a411aaf0650c36aa163ed9b1c2984b836b

Fix asset rows not being memoized (#12029)
- Fix asset rows no longer being memoized
- Regression *likely* introduced by #11937
- The fix (`[...columns]` in `AssetsTable.tsx`) is unrelated though which is weird

# Important Notes
None

app/gui/src/dashboard/layouts/AssetsTable.tsx
@@ -146,6 +146,12 @@ import type { SortInfo } from '#/utilities/sorting'
 import { twJoin, twMerge } from '#/utilities/tailwindMerge'
 import Visibility from '#/utilities/Visibility'
 import invariant from 'tiny-invariant'
+import {
+  SUGGESTIONS_FOR_HAS,
+  SUGGESTIONS_FOR_NEGATIVE_TYPE,
+  SUGGESTIONS_FOR_NO,
+  SUGGESTIONS_FOR_TYPE,
+} from './Drive/suggestionsConstants'
 
 declare module '#/utilities/LocalStorage' {
   /** */
@@ -170,93 +176,6 @@ const MINIMUM_DROPZONE_INTERSECTION_RATIO = 0.5
 const ROW_HEIGHT_PX = 36
 /** The size of the loading spinner. */
 const LOADING_SPINNER_SIZE_PX = 36
-
-const SUGGESTIONS_FOR_NO: assetSearchBar.Suggestion[] = [
-  {
-    key: 'no:label',
-    render: () => 'no:label',
-    addToQuery: (query) => query.addToLastTerm({ nos: ['label'] }),
-    deleteFromQuery: (query) => query.deleteFromLastTerm({ nos: ['label'] }),
-  },
-  {
-    key: 'no:description',
-    render: () => 'no:description',
-    addToQuery: (query) => query.addToLastTerm({ nos: ['description'] }),
-    deleteFromQuery: (query) => query.deleteFromLastTerm({ nos: ['description'] }),
-  },
-]
-const SUGGESTIONS_FOR_HAS: assetSearchBar.Suggestion[] = [
-  {
-    key: 'has:label',
-    render: () => 'has:label',
-    addToQuery: (query) => query.addToLastTerm({ negativeNos: ['label'] }),
-    deleteFromQuery: (query) => query.deleteFromLastTerm({ negativeNos: ['label'] }),
-  },
-  {
-    key: 'has:description',
-    render: () => 'has:description',
-    addToQuery: (query) => query.addToLastTerm({ negativeNos: ['description'] }),
-    deleteFromQuery: (query) => query.deleteFromLastTerm({ negativeNos: ['description'] }),
-  },
-]
-const SUGGESTIONS_FOR_TYPE: assetSearchBar.Suggestion[] = [
-  {
-    key: 'type:project',
-    render: () => 'type:project',
-    addToQuery: (query) => query.addToLastTerm({ types: ['project'] }),
-    deleteFromQuery: (query) => query.deleteFromLastTerm({ types: ['project'] }),
-  },
-  {
-    key: 'type:folder',
-    render: () => 'type:folder',
-    addToQuery: (query) => query.addToLastTerm({ types: ['folder'] }),
-    deleteFromQuery: (query) => query.deleteFromLastTerm({ types: ['folder'] }),
-  },
-  {
-    key: 'type:file',
-    render: () => 'type:file',
-    addToQuery: (query) => query.addToLastTerm({ types: ['file'] }),
-    deleteFromQuery: (query) => query.deleteFromLastTerm({ types: ['file'] }),
-  },
-  {
-    key: 'type:secret',
-    render: () => 'type:secret',
-    addToQuery: (query) => query.addToLastTerm({ types: ['secret'] }),
-    deleteFromQuery: (query) => query.deleteFromLastTerm({ types: ['secret'] }),
-  },
-  {
-    key: 'type:datalink',
-    render: () => 'type:datalink',
-    addToQuery: (query) => query.addToLastTerm({ types: ['datalink'] }),
-    deleteFromQuery: (query) => query.deleteFromLastTerm({ types: ['datalink'] }),
-  },
-]
-const SUGGESTIONS_FOR_NEGATIVE_TYPE: assetSearchBar.Suggestion[] = [
-  {
-    key: 'type:project',
-    render: () => 'type:project',
-    addToQuery: (query) => query.addToLastTerm({ negativeTypes: ['project'] }),
-    deleteFromQuery: (query) => query.deleteFromLastTerm({ negativeTypes: ['project'] }),
-  },
-  {
-    key: 'type:folder',
-    render: () => 'type:folder',
-    addToQuery: (query) => query.addToLastTerm({ negativeTypes: ['folder'] }),
-    deleteFromQuery: (query) => query.deleteFromLastTerm({ negativeTypes: ['folder'] }),
-  },
-  {
-    key: 'type:file',
-    render: () => 'type:file',
-    addToQuery: (query) => query.addToLastTerm({ negativeTypes: ['file'] }),
-    deleteFromQuery: (query) => query.deleteFromLastTerm({ negativeTypes: ['file'] }),
-  },
-  {
-    key: 'type:datalink',
-    render: () => 'type:datalink',
-    addToQuery: (query) => query.addToLastTerm({ negativeTypes: ['datalink'] }),
-    deleteFromQuery: (query) => query.deleteFromLastTerm({ negativeTypes: ['datalink'] }),
-  },
-]
 
 /** Information related to a drag selection. */
 interface DragSelectionInfo {
@@ -803,7 +722,7 @@ function AssetsTable(props: AssetsTableProps) {
     }
   }, [navigator2D, setMostRecentlySelectedIndex])
 
-  const onKeyDown = (event: KeyboardEvent) => {
+  const onKeyDown = useEventCallback((event: KeyboardEvent) => {
     const { selectedAssets } = driveStore.getState()
     const prevIndex = mostRecentlySelectedIndexRef.current
     const item = prevIndex == null ? null : visibleItems[prevIndex]
@@ -983,7 +902,7 @@ function AssetsTable(props: AssetsTableProps) {
         break
       }
     }
-  }
+  })
 
   useEffect(() => {
     const onClick = () => {
@@ -1087,22 +1006,38 @@ function AssetsTable(props: AssetsTableProps) {
     setEnabledColumns((currentColumns) => withPresence(currentColumns, column, false))
   })
 
-  const state: AssetsTableState = {
-    backend,
-    rootDirectoryId,
-    scrollContainerRef: rootRef,
-    category,
-    sortInfo,
-    setSortInfo,
-    query,
-    setQuery,
-    nodeMap: nodeMapRef,
-    hideColumn,
-    doCopy,
-    doCut,
-    doPaste,
-    getAssetNodeById,
-  }
+  const state: AssetsTableState = useMemo(
+    () => ({
+      backend,
+      rootDirectoryId,
+      scrollContainerRef: rootRef,
+      category,
+      sortInfo,
+      setSortInfo,
+      query,
+      setQuery,
+      nodeMap: nodeMapRef,
+      hideColumn,
+      doCopy,
+      doCut,
+      doPaste,
+      getAssetNodeById,
+    }),
+    [
+      backend,
+      category,
+      doCopy,
+      doCut,
+      doPaste,
+      getAssetNodeById,
+      hideColumn,
+      nodeMapRef,
+      query,
+      rootDirectoryId,
+      setQuery,
+      sortInfo,
+    ],
+  )
 
   useEffect(() => {
     // In some browsers, at least in Chrome 126,
@@ -1412,7 +1347,8 @@ function AssetsTable(props: AssetsTableProps) {
 
   const headerRow = (
     <tr ref={headerRowRef} className="rounded-none text-sm font-semibold">
-      {columns.map((column) => {
+      {[...columns].map((column) => {
+        // The spread on the line above is required for React Compiler to compile this component.
         // This is a React component, even though it does not contain JSX.
         const Heading = COLUMN_HEADING[column]
 

app/gui/src/dashboard/layouts/Drive/assetTreeHooks.tsx
@@ -125,7 +125,7 @@ export function useAssetTree(options: UseAssetTreeOptions) {
        * in the loaded data. If it is loaded, we append that data to the asset node
        * and do the same for the children.
        */
-      const withChildren = (node: AnyAssetTreeNode, depth: number) => {
+      const withChildren = (node: AnyAssetTreeNode, depth: number): AnyAssetTreeNode => {
         const { item } = node
 
         if (assetIsDirectory(item)) {
@@ -136,19 +136,19 @@ export function useAssetTree(options: UseAssetTreeOptions) {
           )
 
           if (childrenAssetsQuery == null || childrenAssetsQuery.isLoading) {
-            node = node.with({
+            return node.with({
               children: [AssetTreeNode.fromAsset(createSpecialLoadingAsset(item.id), depth, '')],
             })
           } else if (childrenAssetsQuery.isError) {
-            node = node.with({
+            return node.with({
               children: [AssetTreeNode.fromAsset(createSpecialErrorAsset(item.id), depth, '')],
             })
           } else if (nestedChildren?.length === 0) {
-            node = node.with({
+            return node.with({
               children: [AssetTreeNode.fromAsset(createSpecialEmptyAsset(item.id), depth, '')],
             })
           } else if (nestedChildren != null) {
-            node = node.with({
+            return node.with({
               children: nestedChildren.map((child) => withChildren(child, depth + 1)),
             })
           }

app/gui/src/dashboard/layouts/Drive/assetsTableItemsHooks.tsx
@@ -1,6 +1,4 @@
 /** @file A hook to return the items in the assets table. */
-import { useMemo } from 'react'
-
 import type { AnyAsset, AssetId } from 'enso-common/src/services/Backend'
 import { AssetType, getAssetPermissionName } from 'enso-common/src/services/Backend'
 import { PermissionAction } from 'enso-common/src/utilities/permissions'
@@ -65,7 +63,7 @@ export function useAssetsTableItems(options: UseAssetsTableOptions) {
 
   const setAssetItems = useStore(ASSET_ITEMS_STORE, (store) => store.setItems)
 
-  const filter = useMemo(() => {
+  const filter = (() => {
     const globCache: Record<string, RegExp> = {}
     if (/^\s*$/.test(query.query)) {
       return null
@@ -171,9 +169,9 @@ export function useAssetsTableItems(options: UseAssetsTableOptions) {
         )
       }
     }
-  }, [query])
+  })()
 
-  const visibilities = useMemo(() => {
+  const visibilities = (() => {
     const map = new Map<AssetId, Visibility>()
 
     const processNode = (node: AnyAssetTreeNode) => {
@@ -204,9 +202,9 @@ export function useAssetsTableItems(options: UseAssetsTableOptions) {
     processNode(assetTree)
 
     return map
-  }, [assetTree, filter])
+  })()
 
-  const displayItems = useMemo(() => {
+  const displayItems = (() => {
     if (sortInfo == null) {
       const flatTree = assetTree.preorderTraversal((children) =>
         children.filter((child) => expandedDirectoryIds.includes(child.item.parentId)),
@@ -238,13 +236,12 @@ export function useAssetsTableItems(options: UseAssetsTableOptions) {
 
       return flatTree
     }
-  }, [sortInfo, assetTree, expandedDirectoryIds])
+  })()
 
   setAssetItems(displayItems.map((item) => item.item))
 
-  const visibleItems = useMemo(
-    () => displayItems.filter((item) => visibilities.get(item.item.id) !== Visibility.hidden),
-    [displayItems, visibilities],
+  const visibleItems = displayItems.filter(
+    (item) => visibilities.get(item.item.id) !== Visibility.hidden,
   )
 
   return { visibilities, displayItems, visibleItems } as const

app/gui/src/dashboard/layouts/Drive/suggestionsConstants.ts
@@ -1,0 +1,89 @@
+/** @file Constants related to suggestions for the asset search bar. */
+import type * as assetSearchBar from '#/layouts/AssetSearchBar'
+
+export const SUGGESTIONS_FOR_NO: assetSearchBar.Suggestion[] = [
+  {
+    key: 'no:label',
+    render: () => 'no:label',
+    addToQuery: (query) => query.addToLastTerm({ nos: ['label'] }),
+    deleteFromQuery: (query) => query.deleteFromLastTerm({ nos: ['label'] }),
+  },
+  {
+    key: 'no:description',
+    render: () => 'no:description',
+    addToQuery: (query) => query.addToLastTerm({ nos: ['description'] }),
+    deleteFromQuery: (query) => query.deleteFromLastTerm({ nos: ['description'] }),
+  },
+]
+export const SUGGESTIONS_FOR_HAS: assetSearchBar.Suggestion[] = [
+  {
+    key: 'has:label',
+    render: () => 'has:label',
+    addToQuery: (query) => query.addToLastTerm({ negativeNos: ['label'] }),
+    deleteFromQuery: (query) => query.deleteFromLastTerm({ negativeNos: ['label'] }),
+  },
+  {
+    key: 'has:description',
+    render: () => 'has:description',
+    addToQuery: (query) => query.addToLastTerm({ negativeNos: ['description'] }),
+    deleteFromQuery: (query) => query.deleteFromLastTerm({ negativeNos: ['description'] }),
+  },
+]
+export const SUGGESTIONS_FOR_TYPE: assetSearchBar.Suggestion[] = [
+  {
+    key: 'type:project',
+    render: () => 'type:project',
+    addToQuery: (query) => query.addToLastTerm({ types: ['project'] }),
+    deleteFromQuery: (query) => query.deleteFromLastTerm({ types: ['project'] }),
+  },
+  {
+    key: 'type:folder',
+    render: () => 'type:folder',
+    addToQuery: (query) => query.addToLastTerm({ types: ['folder'] }),
+    deleteFromQuery: (query) => query.deleteFromLastTerm({ types: ['folder'] }),
+  },
+  {
+    key: 'type:file',
+    render: () => 'type:file',
+    addToQuery: (query) => query.addToLastTerm({ types: ['file'] }),
+    deleteFromQuery: (query) => query.deleteFromLastTerm({ types: ['file'] }),
+  },
+  {
+    key: 'type:secret',
+    render: () => 'type:secret',
+    addToQuery: (query) => query.addToLastTerm({ types: ['secret'] }),
+    deleteFromQuery: (query) => query.deleteFromLastTerm({ types: ['secret'] }),
+  },
+  {
+    key: 'type:datalink',
+    render: () => 'type:datalink',
+    addToQuery: (query) => query.addToLastTerm({ types: ['datalink'] }),
+    deleteFromQuery: (query) => query.deleteFromLastTerm({ types: ['datalink'] }),
+  },
+]
+export const SUGGESTIONS_FOR_NEGATIVE_TYPE: assetSearchBar.Suggestion[] = [
+  {
+    key: 'type:project',
+    render: () => 'type:project',
+    addToQuery: (query) => query.addToLastTerm({ negativeTypes: ['project'] }),
+    deleteFromQuery: (query) => query.deleteFromLastTerm({ negativeTypes: ['project'] }),
+  },
+  {
+    key: 'type:folder',
+    render: () => 'type:folder',
+    addToQuery: (query) => query.addToLastTerm({ negativeTypes: ['folder'] }),
+    deleteFromQuery: (query) => query.deleteFromLastTerm({ negativeTypes: ['folder'] }),
+  },
+  {
+    key: 'type:file',
+    render: () => 'type:file',
+    addToQuery: (query) => query.addToLastTerm({ negativeTypes: ['file'] }),
+    deleteFromQuery: (query) => query.deleteFromLastTerm({ negativeTypes: ['file'] }),
+  },
+  {
+    key: 'type:datalink',
+    render: () => 'type:datalink',
+    addToQuery: (query) => query.addToLastTerm({ negativeTypes: ['datalink'] }),
+    deleteFromQuery: (query) => query.deleteFromLastTerm({ negativeTypes: ['datalink'] }),
+  },
+]